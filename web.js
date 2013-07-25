e7bdf4d6a90f587da1b202190f1399cfeb244ead
Ubuntu <ubuntu@ip-172-31-40-231.us-west-2.compute.internal>
2013-07-25 04:21:51 +0000

    I think this one works

--- web.js
+++ web.js
@@ -4,7 +4,10 @@ var fs = require('fs');
 app.use(express.logger());
 var k = fs.readFileSync('./index.html','utf8');
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
  var k = fs.readFileSync('./index.html','utf8');
- response.send( k );
+ res.send('test');
 });
+
+app.listen(5000);
+console.log('Listening on port 5000');
