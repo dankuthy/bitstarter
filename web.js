12d521814f8bfb8be1074ff51473151b427ba926
Ubuntu <ubuntu@ip-172-31-40-231.us-west-2.compute.internal>
2013-07-25 03:48:54 +0000

    Another commit

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
-var file = fs.readFileSync('./index.html,'utf8');
-
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(file);
+ var k = fs.readFileSync(./index.html','utf8');
+ response.send( k );
 });
 
 var port = process.env.PORT || 5000;
